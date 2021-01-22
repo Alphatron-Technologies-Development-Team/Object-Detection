718c3ff53dae52a3413dbce96d495cb20ac38abc
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2021-01-23 03:49:50 +0500

    Real_Time Object Detection

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import project_file as pf
-pf.Object_Detection()
+import real_time as rt
+user_input = int(input('Press 1 for single image testing\nPress 2 for Real Time Testing'))
+if user_input == 1:
+    pf.Object_Detection()
+elif user_input == 2:
+    rt.Object_Detection_RealTime()
