6ac530153dcdf3f69891e53854876bcf47f670e5
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2021-01-23 04:45:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import project_file as pf
 import real_time as rt
-user_input = int(input('Press 1 for single image testing\nPress 2 for Real Time Testing'))
+user_input = int(input('Press 1 for single image testing\nPress 2 for Real Time Testing\n'))
 if user_input == 1:
     pf.Object_Detection()
 elif user_input == 2:
